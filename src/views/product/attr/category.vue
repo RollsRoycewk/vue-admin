66ad6595b2d836b3c481bca5581fd1e4a6a3adf8
RollsRoycewk <50765560@qq.com>
2020-12-12 16:13:46 +0800

    feat:(category)二级三级数据展示完毕

--- src/views/product/attr/category.vue
+++ src/views/product/attr/category.vue
@@ -3,7 +3,11 @@
   <el-card>
     <el-form :inline="true" :model="category" class="demo-form-inline">
       <el-form-item label="一级分类">
-        <el-select v-model="category.category1Id" placeholder="请选择">
+        <el-select
+          v-model="category.category1Id"
+          placeholder="请选择"
+          @change="handleSelectChange1"
+        >
           <!-- 一级数据展示 -->
           <el-option
             :label="c1.name"
@@ -14,15 +18,29 @@
         </el-select>
       </el-form-item>
       <el-form-item label="二级分类">
-        <el-select v-model="category.category2Id" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+        <el-select
+          v-model="category.category2Id"
+          placeholder="请选择"
+          @change="handleSelectChange2"
+        >
+          <!-- 二级分类数据 -->
+          <el-option
+            :label="c2.name"
+            :value="c2.id"
+            v-for="c2 in category2List"
+            :key="c2.id"
+          ></el-option>
         </el-select>
       </el-form-item>
       <el-form-item label="三级分类">
-        <el-select v-model="category.category2Id" placeholder="请选择">
-          <el-option label="区域一" value="shanghai"></el-option>
-          <el-option label="区域二" value="beijing"></el-option>
+        <el-select v-model="category.category3Id" placeholder="请选择">
+          <!-- 三级分类数据 -->
+          <el-option
+            :label="c3.name"
+            :value="c3.id"
+            v-for="c3 in category3List"
+            :key="c3.id"
+          ></el-option>
         </el-select>
       </el-form-item>
     </el-form>
@@ -40,11 +58,37 @@ export default {
         category3Id: "",
       },
       category1List: [],
+      category2List: [],
+      category3List: [],
     };
+  },
+  methods: {
+    // 点击一级目录,获取二级目录
+    async handleSelectChange1(e) {
+      // console.log(e); 获取的是一级目录的id
+      const res = await this.$API.attrs.getCategory2Data(e);
+      if (res.ok) {
+        this.$message.success("二级分类数据获取成功");
+        this.category2List = res.data;
+      } else {
+        this.$message.error("二级分类数据获取失败");
+      }
+    },
+
+    // 点击二级目录,获取三级目录
+    async handleSelectChange2(e) {
+      // console.log(e); 获取的是一级目录的id
+      const res = await this.$API.attrs.getCategory3Data(e);
+      if (res.ok) {
+        this.$message.success("三级分类数据获取成功");
+        this.category3List = res.data;
+      } else {
+        this.$message.error("三级分类数据获取失败");
+      }
+    },
   },
   async mounted() {
     const res = await this.$API.attrs.getCategory1Data();
-    console.log(res);
     if (res.ok) {
       this.$message.success("一级分类数据获取成功");
       this.category1List = res.data;
