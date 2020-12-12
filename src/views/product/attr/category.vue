72dbe5cbca816376d6d1afe651ba0144a015efd1
RollsRoycewk <50765560@qq.com>
2020-12-12 14:37:35 +0800

    feat:(attr)实现组件复用

---
+++ src/views/product/attr/category.vue
@@ -0,0 +1,43 @@
+<template>
+  <!-- 选择 -->
+  <el-card>
+    <el-form :inline="true" :model="category" class="demo-form-inline">
+      <el-form-item label="一级分类">
+        <el-select v-model="category.category1Id" placeholder="请选择">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="二级分类">
+        <el-select v-model="category.category2Id" placeholder="请选择">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+      <el-form-item label="三级分类">
+        <el-select v-model="category.category2Id" placeholder="请选择">
+          <el-option label="区域一" value="shanghai"></el-option>
+          <el-option label="区域二" value="beijing"></el-option>
+        </el-select>
+      </el-form-item>
+    </el-form>
+  </el-card>
+</template>
+
+<script>
+export default {
+  name: "Category",
+  data() {
+    return {
+      category: {
+        category1Id: "",
+        category2Id: "",
+        category3Id: "",
+      },
+    };
+  },
+};
+</script>
+
+<style>
+</style>
--- src/views/product/attr/list.vue
+++ src/views/product/attr/list.vue
@@ -1,28 +1,7 @@
 <template>
   <div>
-    <!-- 选择 -->
-    <el-card>
-      <el-form :inline="true" :model="category" class="demo-form-inline">
-        <el-form-item label="一级分类">
-          <el-select v-model="category.category1Id" placeholder="请选择">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="二级分类">
-          <el-select v-model="category.category2Id" placeholder="请选择">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="三级分类">
-          <el-select v-model="category.category2Id" placeholder="请选择">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
-          </el-select>
-        </el-form-item>
-      </el-form>
-    </el-card>
+    <!-- 组件 -->
+    <Category></Category>
     <!-- 信息 -->
     <el-card class="box-card">
       <!-- 按钮 -->
@@ -45,17 +24,18 @@
 </template>
 
 <script>
+// 引入组件
+import Category from "./category";
+
 export default {
   name: "AttrList",
   data() {
     return {
-      category: {
-        category1Id: "",
-        category2Id: "",
-        category3Id: "",
-      },
       attrList: [],
     };
+  },
+  components: {
+    Category,
   },
 };
 </script>
